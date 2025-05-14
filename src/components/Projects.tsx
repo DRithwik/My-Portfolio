d01065dcb50240d8f2ab565717ced80d52ea639c
Rithwik Deshaboina <168298268+DRithwik@users.noreply.github.com>
2025-05-14 16:01:16 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -16,16 +16,16 @@ const projects = [
     ]
   },
   {
-    title: 'Event Registration System',
+    title: 'Deep Research AI Agent',
     desc: '',
-    tech: ['React.js', 'Node.js', 'MongoDB', 'Express.js'],
-    image: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&q=80',
-    link: 'https://github.com/DRithwik/Event-Registration-System-WTS',
+    tech: ['Next.js 15', 'GPT-4o', 'Exa Search API', 'TypeScript'],
+    image: 'https://media-hosting.imagekit.io/1573d4fb17df4e3b/Final%20output%20with%20report.jpg?Expires=1841826615&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=HvEszYPaT2XyZPRtfUnD20ctzruJ5S4bnfaZvNdK33kKw3XwfI96Zt88WnnOYFP6sjhDjshyRnYuZ7hOXHsUXx5vLiXqszZFYaDvFRWEhE3Ix6YtQWvfSONUO8S-N6dTV1vQz9jAUZmyszPZgjUYGutXPqYF2aOO6XJEDuSVVqwgx8ZinwGVKPyDC6SCCPj6X3XLkJWmeIkwj3Phv8lmkoesRt6uGbnIHQ0qPuiOyjthZJc6sCgOF6IqdO7nK3Brhi5XwRD~ofrksGJSYBLAOYDJlRKqmIW3lz1nxtt5taGAiYecqmQRqKtba9HVSYLLFmvr3A33uItsa7~~z0ophA__',
+    link: 'https://github.com/DRithwik/Deep-Research-AI-Agent',
     points: [
-      'Developed RESTful APIs for event management',
-      'Implemented real-time availability checking',
-      'Added secure payment gateway integration',
-      'Built an admin dashboard for event management'
+      'Built an AI research agent with Next.js, Vercel SDK, and multiple LLMs.',
+      'Integrated Exa Search API for enhanced data sourcing.',
+      'Enabled customizable, iterative research workflows.',
+      'Used modular UI components with Tailwind and Shadcn UI.'
     ]
   },
   {
