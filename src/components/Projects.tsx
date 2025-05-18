a7fd77d8c79f6572e39853d30fd37dfbd5df8d05
Rithwik Deshaboina <168298268+DRithwik@users.noreply.github.com>
2025-05-18 15:27:08 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -19,7 +19,7 @@ const projects = [
     title: 'Deep Research AI Agent',
     desc: '',
     tech: ['Next.js', 'GPT-4o', 'Exa Search API', 'TypeScript'],
-    image: 'https://media-hosting.imagekit.io/1573d4fb17df4e3b/Final%20output%20with%20report.jpg?Expires=1841826615&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=HvEszYPaT2XyZPRtfUnD20ctzruJ5S4bnfaZvNdK33kKw3XwfI96Zt88WnnOYFP6sjhDjshyRnYuZ7hOXHsUXx5vLiXqszZFYaDvFRWEhE3Ix6YtQWvfSONUO8S-N6dTV1vQz9jAUZmyszPZgjUYGutXPqYF2aOO6XJEDuSVVqwgx8ZinwGVKPyDC6SCCPj6X3XLkJWmeIkwj3Phv8lmkoesRt6uGbnIHQ0qPuiOyjthZJc6sCgOF6IqdO7nK3Brhi5XwRD~ofrksGJSYBLAOYDJlRKqmIW3lz1nxtt5taGAiYecqmQRqKtba9HVSYLLFmvr3A33uItsa7~~z0ophA__',
+    image: 'https://i.ibb.co/N2JVPMLp/Final-output-with-report.png',
     link: 'https://github.com/DRithwik/Deep-Research-AI-Agent',
     points: [
       'Built an AI agent with Next.js & multi-LLM support.',
