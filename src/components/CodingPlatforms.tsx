8751ced8af4ec87e0be9e206cc854ade08561ba9
Rithwik Deshaboina <168298268+DRithwik@users.noreply.github.com>
2025-06-03 18:50:18 +0530

    Update CodingPlatforms.tsx

--- src/components/CodingPlatforms.tsx
+++ src/components/CodingPlatforms.tsx
@@ -7,13 +7,13 @@ export default function CodingPlatforms() {
       name: 'GeeksforGeeks',
       username: 'rithwik_d',
       url: 'https://auth.geeksforgeeks.org/user/rithwik_d',
-      achievements: 'Rank 1 in Institute (VCE), 390+ Days Streak'
+      achievements: 'Rank 1 in Institute (VCE), 400+ Days Streak'
     },
     {
       name: 'CodeChef',
       username: 'rithwik007',
       url: 'https://www.codechef.com/users/rithwik007',
-      achievements: '330+ Days Streak'
+      achievements: '340+ Days Streak'
     },
     {
       name: 'LeetCode',
