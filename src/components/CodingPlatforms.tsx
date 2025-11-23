b2878d46cacd4c86b71bce657c0352feb2a87442
Rithwik Deshaboina <168298268+DRithwik@users.noreply.github.com>
2025-11-23 06:58:39 -0800

    Update CodingPlatforms.tsx

--- src/components/CodingPlatforms.tsx
+++ src/components/CodingPlatforms.tsx
@@ -13,7 +13,7 @@ export default function CodingPlatforms() {
       name: 'InterviewBit',
       username: 'rithwik-d',
       url: 'https://www.interviewbit.com/profile/rithwik-d/',
-      achievements: 'Global Rank #105'
+      achievements: 'Global Rank #50'
     },
     {
       name: 'Coding Ninjas',
