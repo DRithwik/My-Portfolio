4b6913868a244e3290c5b90bf831ac9f3901b26c
Rithwik Deshaboina <168298268+DRithwik@users.noreply.github.com>
2025-11-23 07:00:27 -0800

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -62,6 +62,10 @@ export default function About() {
                     <span className="w-2 h-2 bg-blue-400 rounded-full"></span>
                     <p className="text-gray-300">Letter of Recommendation from IIT Bombay</p>
                   </div>
+                 <div className="flex items-center gap-2">
+                    <span className="w-2 h-2 bg-blue-400 rounded-full"></span>
+                    <p className="text-gray-300">Interviewbit Global Rank : 50</p>
+                  </div>
                   <div className="flex items-center gap-2">
                     <span className="w-2 h-2 bg-blue-400 rounded-full"></span>
                     <p className="text-gray-300">Full Stack Developer Intern at TechnoHacks Solution Pvt. Ltd.</p>
